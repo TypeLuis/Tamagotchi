8f4e5a85220e1fad6109d8e0a543407e5c1af821
typeluis <typeluis@gmail.com>
2021-10-23 00:43:40 -0400

    progress 6

--- main.js
+++ main.js
@@ -15,14 +15,21 @@ const tamagachi = document.getElementById('tamagachi')
 const bubbles = document.getElementsByClassName('bubbles')
 let gameLost = false
 
-let colorPrompt = prompt("What color would you like your tamagachi?")
+// let colorPrompt = prompt("What color would you like your tamagachi?")
 
-for (bubble of bubbles){
-    bubble.style.backgroundColor = colorPrompt
+// for (bubble of bubbles){
+//     bubble.style.backgroundColor = colorPrompt
+// }
+
+const chopperUrl = {
+    hungry: '/assets/maxresdefault.png',
+    bored: '/assets/anime-doctor-one-piece-pirates-wallpaper-thumb.png',
+    sleep: '/assets/d5e14q8-2421a2c9-76a7-48b7-84a1-cabc5b8c8840 (1).png',
+    up: '/assets/5-tony-tony-chopper-sreeraj-kaniyamparambil-transparent.png',
+    eat: '/assets/27704659aae4b05f53c86950de2c8fb5.png',
+    fun: '/assets/9039f5ca23b28b74c45bf712848ee3e0.png',
+    start: '/assets/Chopper_post_timeskip.png'
 }
-
-
-
 
 const numEls = document.getElementsByClassName('number')
 
@@ -30,12 +37,12 @@ const numEls = document.getElementsByClassName('number')
 
 
 
+const chopImg = document.getElementById('chopper')
 
 
 
 
-
-function interval(element, button, baseNum){
+function interval(element, button, baseNum, src, id){
     // model - bindings: variables, DOM references, other values related to application state 
     let interval;
     let count = 1
@@ -56,6 +63,11 @@ function interval(element, button, baseNum){
     function buttonPress(){
         button.addEventListener('click', () =>{
             if(element.innerText > 0 && gameLost === false){
+                chopImg.removeAttribute('src')
+                chopImg.removeAttribute('id')
+                chopImg.setAttribute('id', id)
+                chopImg.setAttribute('src', src)
+                console.log(src)
                 return element.innerText --  
             }          
         })
@@ -98,6 +110,6 @@ function interval(element, button, baseNum){
     // number - interger (in milliseconds)
 }
 
-interval(boredNum, playBtn, 500)
-interval(hungerNum, feedBtn, 750)
-interval(tiredNum, sleepBtn, 500)
+interval(boredNum, playBtn, 500, chopperUrl['fun'], "chop")
+interval(hungerNum, feedBtn, 750, chopperUrl['eat'], "chop2")
+interval(tiredNum, sleepBtn, 500, chopperUrl['up'], "chop3")
